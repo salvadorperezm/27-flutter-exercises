a82da13959b9b3dfdd3e416adb7a15156addefdd
Salvador Pérez <71912670+salvadorperezm@users.noreply.github.com>
2023-09-04 17:12:26 -0600

    Initial commit

---
+++ bin/dart_practice_exercises.dart
@@ -0,0 +1,5 @@
+import 'package:dart_practice_exercises/dart_practice_exercises.dart' as dart_practice_exercises;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_practice_exercises.calculate()}!');
+}
